ffb8bcd511333e8dbfd4ea1ecc6af9fd3d554b4a
Daniel Beneyto <danfaizer@gmail.com>
2020-03-27 18:26:11 +0100

    Allow http endpoint as jobqueue name

--- lib/sqs_service.rb
+++ lib/sqs_service.rb
@@ -1,6 +1,14 @@
 require 'net/http'
 require 'aws-sdk-rails'
 require 'json'
+require 'uri'
+
+def valid_url(url)
+  uri = URI.parse(url)
+  uri.is_a?(URI::HTTP) && !uri.host.nil?
+rescue URI::InvalidURIError
+  false
+end
 
 class SQSService
   def initialize(sqs)
@@ -8,10 +16,6 @@ class SQSService
   end
 
   def enqueue(check, start_time)
-    resp = @sqs.get_queue_url({
-      queue_name: check.queue_name,
-    })
-
     check_message = {
       "check_id" => check.id,
       "target" => check.target,
@@ -23,9 +27,33 @@ class SQSService
       "start_time" => start_time
     }
 
-    resp = @sqs.send_message({
-      queue_url: resp.queue_url,
-      message_body: check_message.to_json,
-    })
+    # Obtain queue url.
+    queue_url = ""
+    if valid_url(check.queue_name)
+      queue_url = check.queue_name
+    else
+      begin
+        resp = @sqs.get_queue_url({
+          queue_name: check.queue_name,
+        })
+        queue_url = resp.queue_url
+      rescue => e
+        Rails.logger.error "error obtaining queue url for check: #{check.id} for queue #{check.queue_name}"
+        Rails.logger.error e.message
+        return
+      end
+    end
+
+    # Publish check to queue.
+    begin
+      resp = @sqs.send_message({
+        queue_url: queue_url,
+        message_body: check_message.to_json,
+      })
+    rescue => e
+      Rails.logger.error "error queueing check: #{check.id} to queue #{queue_url}"
+      Rails.logger.error e.message
+      return
+    end
   end
 end
