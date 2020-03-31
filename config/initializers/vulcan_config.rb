47dbd28bf3a9007723d8d804ff5f6c607b6e540b
Daniel Beneyto <danfaizer@gmail.com>
2020-03-31 09:02:50 +0200

    Improve Nessus queue env fetch and default

--- config/initializers/vulcan_config.rb
+++ config/initializers/vulcan_config.rb
@@ -3,8 +3,4 @@ Rails.application.config.region = ENV.fetch('REGION')
 Rails.application.config.scans_bucket = ENV.fetch('SCANS_BUCKET')
 Rails.application.config.scan_processor_buffer = ENV.fetch('SCAN_PROCESSOR_BUFFER_SIZE_MB')
 Rails.application.config.sns_topic_arn = ENV.fetch('SNS_TOPIC_ARN')
-begin
-  Rails.application.config.nessus_check_queue = ENV.fetch('NESSUS_CHECK_QUEUE')
-rescue
-  Rails.application.config.nessus_check_queue = "default"
-end
+Rails.application.config.nessus_check_queue = ENV['NESSUS_CHECK_QUEUE'] || "default"
